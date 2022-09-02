83993ba93e818845df57038cdf64dd14704854f0
van <nguyenhuuvan26101998@gmail.com>
2022-09-03 00:12:25 +0700

    Refactor: move UserNotFoundException to exception package

--- 1-restful-web-services/src/main/java/com/nhvanse/restfulwebservices/social/exception/CustomizedResponseEntityExceptionHandler.java
+++ 1-restful-web-services/src/main/java/com/nhvanse/restfulwebservices/social/exception/CustomizedResponseEntityExceptionHandler.java
@@ -1,6 +1,5 @@
 package com.nhvanse.restfulwebservices.social.exception;
 
-import com.nhvanse.restfulwebservices.social.UserNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
--- 1-restful-web-services/src/main/java/com/nhvanse/restfulwebservices/social/exception/UserNotFoundException.java
+++ 1-restful-web-services/src/main/java/com/nhvanse/restfulwebservices/social/exception/UserNotFoundException.java
@@ -1,4 +1,4 @@
-package com.nhvanse.restfulwebservices.social;
+package com.nhvanse.restfulwebservices.social.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
