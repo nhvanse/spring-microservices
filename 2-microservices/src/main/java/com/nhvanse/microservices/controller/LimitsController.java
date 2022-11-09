f3a1eb0a52d82bd0ceed0da65de2cc65670ae909
van <nguyenhuuvan26101998@gmail.com>
2022-11-10 00:28:08 +0700

    feat: update limits service - add Configuration

--- 2-microservices/src/main/java/com/nhvanse/microservices/controller/LimitsController.java
+++ 2-microservices/src/main/java/com/nhvanse/microservices/controller/LimitsController.java
@@ -1,14 +1,19 @@
 package com.nhvanse.microservices.controller;
 
 import com.nhvanse.microservices.bean.Limits;
+import com.nhvanse.microservices.configuration.Configuration;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LimitsController {
+
+    @Autowired
+    private Configuration configuration;
+
     @GetMapping("/limits")
     public Limits retrieveLimits() {
-
-        return new Limits(1, 1000);
+        return new Limits(configuration.getMinimum(), configuration.getMaximum());
     }
 }
